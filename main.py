b8282aac1053ebd5084cc8a40cb3e4645d7d466f
Gallant <30605394+gallantgoat@users.noreply.github.com>
2025-11-03 18:18:59 -0500

    Created a dictionary to store the current inventory

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ apples_week1 = 12
 
 apples_week2 = 6
 
+invdif_dict = {
+    "apples": 0
+}
+
 def inventory_difference(item_before, item_after):
     if item_after > item_before:
         print("How did you end up with more of this than last week?")
@@ -9,5 +13,7 @@ def inventory_difference(item_before, item_after):
         print("You didn't sell any of this")
     else:
         print(f"You sold {item_before - item_after} of this item")
+        invdif_dict["apples"] = item_before - item_after
+    print(invdif_dict)
 
 inventory_difference(apples_week1, apples_week2)
