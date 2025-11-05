bb0d4f7103a1d21ab2a16c538f61a1ac64a39d4b
Gallant <30605394+gallantgoat@users.noreply.github.com>
2025-11-05 11:40:45 -0500

    Added base required for inventory and inventory item classes

---
+++ inventory.py
@@ -0,0 +1,21 @@
+class InventoryItem:
+    def __init__(self, name, quanity, unit_cost):
+        self.name = name
+        self.quanity = quanity
+        self.unit_cost = unit_cost
+
+class CafeInventory:
+    def __init__(self):
+        self.items = {}
+
+    def add_item():
+        pass
+
+    def change_quanity():
+        pass
+    
+    def inventory_dif():
+        pass 
+
+    def get_low_stock():
+        pass
--- main.py
+++ main.py
@@ -1,19 +1,26 @@
-apples_week1 = 12
+from inventory import Inventory
 
-apples_week2 = 6
+def main():
+    # Input
+    apples_week1 = 12
 
-invdif_dict = {
-    "apples": 0
-}
+    apples_week2 = 6
 
-def inventory_difference(item_before, item_after):
-    if item_after > item_before:
-        print("How did you end up with more of this than last week?")
-    elif item_after == item_before:
-        print("You didn't sell any of this")
-    else:
-        print(f"You sold {item_before - item_after} of this item")
-        invdif_dict["apples"] = item_before - item_after
-    print(invdif_dict)
+    invdif_dict = {
+        "apples": 0
+    }
 
-inventory_difference(apples_week1, apples_week2)
+    def inventory_difference(item_before, item_after):
+        if item_after > item_before:
+            print("How did you end up with more of this than last week?")
+        elif item_after == item_before:
+            print("You didn't sell any of this")
+        else:
+            print(f"You sold {item_before - item_after} of this item")
+            invdif_dict["apples"] = item_before - item_after
+        print(invdif_dict)
+
+    inventory_difference(apples_week1, apples_week2)
+
+if __name__=="__main__":
+    main()
